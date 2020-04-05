0c33ac3b13716afa4c8d63d737fd85a124f56c2d
Rezwan Azfar Haleem <rezwan.haleem@gmail.com>
2020-04-05 17:40:12 -0400

    Section 5 cont.

--- src/index.js
+++ src/index.js
@@ -5,18 +5,27 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({lat: position.coords.latitude});
             },
-            err => console.log(err)
+            err => {
+                this.setState({errorMessage: err.message});
+            }
         );
     }
 
     render() {
-    return <div>Latitude: {this.state.lat}</div>
+        if (this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        else if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        } else {
+            return <div>Loading...</div>
+        }
     }
 }
 
