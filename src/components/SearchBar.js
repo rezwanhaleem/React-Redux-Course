fc68fbd0671b1f1903389ede7b93b188ec2c484c
Rezwan Azfar Haleem <rezwan.haleem@gmail.com>
2020-04-08 16:52:29 -0400

    Section 7 final

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onFormSubmit = e => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term);
+    }
 
     render(){
         return (
             <div className='ui segment'>
-                <form action="" method="get" className='ui form'>
+                <form onSubmit={this.onFormSubmit} method="get" className='ui form'>
                     <div className='field'>           
                         <label htmlFor='search'>Image Search</label>       
-                        <input list="search-items" name="search" autoComplete='off'/>
+                        <input 
+                            list="search-items" 
+                            name="search" 
+                            autoComplete='off' 
+                            onChange={e => this.setState({term: e.target.value})
+                        }/>
                         {/* Datalist may not be fully supported on all browsers. Autocomplete turned off to avoid known bugs */}
                         <datalist id="search-items">
                             <option value="Internet Explorer" />
