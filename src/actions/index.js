88e69c9c21363bc9e1afb01f5f1c02034b07ba9c
Rez <rezwan.haleem@gmail.com>
2020-08-10 19:54:05 -0400

    Section 18 - Redux Thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,7 @@
-//Action Creator
-export const selectSong  = song => {
-    //Return an action
-    return {
-        type: 'SONG_SELECTED',
-        payload: song
-    };
-};
+import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({type: 'FETCH_POSTS', payload: response});
+};
